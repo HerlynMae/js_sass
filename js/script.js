f683649f463261155270510cd4294f8138bcf40c
HerlynMae <torresherlynmae@gmail.com>
2024-01-04 10:27:13 +0800

    not responsive

--- js/script.js
+++ js/script.js
@@ -16,3 +16,35 @@ accordions.forEach((accordion) => {
     accordion.querySelector("i").classList.toggle("open");
   });
 });
+
+const tabsNav = document.querySelectorAll(".tabs__link");
+const tabsContent = document.querySelectorAll(".tabs__image");
+
+tabsNav.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    removeActiveTab();
+    tab.classList.add("active");
+    const activeContent = document.querySelector(`#${tab.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  });
+});
+
+function removeActiveTab() {
+  tabsNav.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  tabsContent.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+}
+
+const spans = document.querySelectorAll(".progress__bar span");
+
+spans.forEach((span) => {
+  span.style.width = span.dataset.width;
+  span.innerHTML = span.dataset.width;
+});
